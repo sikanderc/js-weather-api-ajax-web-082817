478443a2f38ce244666e4e1a99b114c855c16ced
sikanderc <sikanderchowhan@gmail.com>
2017-10-16 16:10:27 -0400

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,39 @@
-function getFahrenheits(result){
-  // Your code goes here
+function getFahrenheits(result) {
+	return result.hourly_forecast.map(function(hour) {
+		return hour.temp.english;
+	});
 }
 
-function getHours(result){
-  // Your code goes here
+function getHours(result) {
+	return result.hourly_forecast.map(function(x) {
+		return x.FCTTIME.hour;
+	});
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+	// Your code goes here
+	return Object.assign(
+		{},
+		{ labels: labels },
+		{
+			datasets: [
+				{
+					label: 'Hourly Weather for New York',
+					fillColor: 'rgba(220,220,220,0.2)',
+					strokeColor: 'rgba(220,220,220,1)',
+					pointColor: 'rgba(220,220,220,1)',
+					pointStrokeColor: '#fff',
+					pointHighlightFill: '#fff',
+					pointHighlightStroke: 'rgba(220,220,220,1)',
+					data: data
+				}
+			]
+		}
+	);
 }
 
-function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+function makeAjaxRequest(endpoint, generateDataSet) {
+	fetch(endpoint, {})
+		.then(res => res.json())
+		.then(json => generateDataSet(json));
 }
